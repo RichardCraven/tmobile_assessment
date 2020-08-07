e76ec5e884694caeb8bc29e3f4fac61e24f25c1b
RichardCraven <rcraven85@gmail.com>
2020-08-06 19:34:14 -0700

    undo implemented, but not with new reducer actions

--- libs/books/feature/src/lib/reading-list/reading-list.component.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
+import { getReadingList, removeFromReadingList, addToReadingList } from '@tmo/books/data-access';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import { ReadingListItem, Book } from '@tmo/shared/models';
 
 @Component({
   selector: 'tmo-reading-list',
@@ -9,10 +11,25 @@ import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
 })
 export class ReadingListComponent {
   readingList$ = this.store.select(getReadingList);
-
-  constructor(private readonly store: Store) {}
+  removed: Book;
+  constructor(private readonly store: Store, private snackBar: MatSnackBar) {}
 
   removeFromReadingList(item) {
+    this.openSnackBar('Undo Remove? ', 'Undo', item)
     this.store.dispatch(removeFromReadingList({ item }));
+  }
+
+  openSnackBar(message: string, action: string, item: ReadingListItem) {
+    const snackBarRef = this.snackBar.open(message, action, {
+      duration: 2000,
+    });
+    snackBarRef.onAction().subscribe(() => {
+      this.store.source.subscribe((e)=>{
+        this.removed = e.books.entities[item.bookId];
+      })
+      setTimeout(()=>{
+        this.store.dispatch(addToReadingList({ book: this.removed }));
+      },0)
+    });
   }
 }
