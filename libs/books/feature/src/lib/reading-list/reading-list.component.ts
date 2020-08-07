0ca6a911d590dc16d4e9dccb5ff880e7a532c18c
RichardCraven <rcraven85@gmail.com>
2020-08-06 22:49:11 -0700

    feature implemented

--- libs/books/feature/src/lib/reading-list/reading-list.component.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
+import { getReadingList, removeFromReadingList, updateReadingList } from '@tmo/books/data-access';
 
 @Component({
   selector: 'tmo-reading-list',
@@ -9,10 +9,22 @@ import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
 })
 export class ReadingListComponent {
   readingList$ = this.store.select(getReadingList);
-
+  hoveredIndex: number;
   constructor(private readonly store: Store) {}
-
   removeFromReadingList(item) {
     this.store.dispatch(removeFromReadingList({ item }));
+  }
+  finishedBook(item){
+    this.store.dispatch(updateReadingList({item}))
+    setTimeout(()=>{
+      this.readingList$ = this.store.select(getReadingList);
+    })
+  }
+  convertToDate(isoDate){
+    const date = new Date(isoDate),
+    year = date.getFullYear(),
+    month = date.getMonth()+1,
+    day = date.getDate();
+    return month + '/' + day + '/' + year
   }
 }
